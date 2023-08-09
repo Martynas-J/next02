b4c74ad92564ae2d12885701c605cd51b5bd4b40
Martynas-J <Aurimhas@gmail.com>
2023-08-10 00:06:18 +0300

    next 04

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+import { notFound } from "next/navigation";
+
+async function getData() {
+    const apiUrl = process.env.API_URL;
+    const res = await fetch(`${apiUrl}/posts`);
+
+    if (!res.ok) {
+        return notFound()
+    }
+
+    return res.json();
 }
 
-export default page
+export const metadata = {
+  title: 'Blog',
+  description: 'Blog Page',
+}
+
+const Blog = async () => {
+  const data = await getData();
+  return (
+    <div className={styles.mainContainer}>
+      {data.map((item) => (
+        <Link href={`/blog/${item._id}`} className={styles.container} key={item._id}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={item.img}
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <h2 className={styles.title}>{item.username}</h2>
+            <p className={styles.desc}>{item.desc}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Blog;

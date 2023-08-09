3f8f9ad1054332a69dd631a602dab7179f12f781
Martynas-J <Aurimhas@gmail.com>
2023-08-09 17:29:17 +0300

    next js test v1.4

--- src/components/GetData/GetData.js
+++ src/components/GetData/GetData.js
@@ -1,9 +1,8 @@
-import { API_URL } from "@/utils/config";
 import { notFound } from "next/navigation";
 
 export default async function getData(id) {
-    
-    const res = await fetch(`${API_URL}/posts/${id}`, {
+    const apiUrl = process.env.API_URL;
+    const res = await fetch(`${apiUrl}/posts/${id}`, {
         cache: "no-store",
     });
 

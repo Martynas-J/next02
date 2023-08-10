6904b8b6b57ec673b13c2bcf0b039fc57ef5ae86
Martynas-J <Aurimhas@gmail.com>
2023-08-10 10:02:50 +0300

    next 11

--- src/components/GetData/GetData.js
+++ src/components/GetData/GetData.js
@@ -2,7 +2,9 @@ import { notFound } from "next/navigation";
 
 export default async function getData(id) {
     const apiUrl = process.env.API_URL;
-    const res = await fetch(`${apiUrl}/api/posts/${id}`);
+    const res = await fetch(`${apiUrl}/api/posts/${id}`, {
+        cache: "no-store",
+    });
 
     if (!res.ok) {
         return notFound()

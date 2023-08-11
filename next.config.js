360c301d49a6564685da593e97c059e53bcf8d52
Martynas-J <Aurimhas@gmail.com>
2023-08-11 09:06:12 +0300

    test same server v2.0

--- next.config.js
+++ next.config.js
@@ -7,11 +7,11 @@ dns.setDefaultResultOrder("ipv4first");
 const nextConfig = {
     env: {
         MONGO: "mongodb+srv://Nordesasdev:Nordesasdev@cluster0.hvglvqz.mongodb.net/test?retryWrites=true&w=majority",
-        API_URL: "https://server02-nu.vercel.app",
+        API_URL: "https://next02-2hoo.vercel.app",
         GOOGLE_ID: "444899498652-lt45t8iafifcm93perrsilj1a44gin08.apps.googleusercontent.com",
         GOOGLE_SECRET: "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",
         NEXTAUTH_SECRET: "qweewqasddsa",
-        NEXTAUTH_URL: "http://localhost:3000",
+        NEXTAUTH_URL: "http://next02-2hoo.vercel.app",
     },
     images: {
         remotePatterns: [

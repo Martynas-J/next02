479786b3700cb422281833b424905fac4fba4a06
Martynas-J <Aurimhas@gmail.com>
2023-08-11 08:12:54 +0300

    test same server v1.8

--- next.config.js
+++ next.config.js
@@ -5,8 +5,21 @@ dns.setDefaultResultOrder("ipv4first");
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+    env: {
+        MONGO: "mongodb+srv://Nordesasdev:Nordesasdev@cluster0.hvglvqz.mongodb.net/test?retryWrites=true&w=majority",
+        API_URL: "http://127.0.0.1:3000",
+        GOOGLE_ID: "444899498652-lt45t8iafifcm93perrsilj1a44gin08.apps.googleusercontent.com",
+        GOOGLE_SECRET: "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",
+        NEXTAUTH_SECRET: "qweewqasddsa",
+        NEXTAUTH_URL: "http://localhost:3000",
+    },
     images: {
-        domains: ["images.pexels.com", "www.pexels.com", "placehold.co"]
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
     },
 };
 

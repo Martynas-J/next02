1a75b9e9f0803758f6769df3941d302be3068516
Martynas-J <Aurimhas@gmail.com>
2023-08-11 07:14:12 +0300

    server

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,55 +1,9 @@
-import React from "react";
-import styles from "./page.module.css";
-import Image from "next/image";
-import getData from "@/components/GetData/GetData";
+import React from 'react'
 
-
-export async function generateMetadata({ params }) {
-
-  const post = await getData(params.id) 
-  return {
-    title: post.title,
-    description: post.desc,   
-  };
+const page = () => {
+  return (
+    <div>page</div>
+  )
 }
 
-const BlogPost = async ({ params }) => {
-  const data = await getData(params.id);
-  return (
-    <div className={styles.container}>
-      <div className={styles.top}>
-        <div className={styles.info}>
-          <h1 className={styles.title}>{data.title}</h1>
-          <p className={styles.desc}>
-            {data.desc}
-          </p>
-          <div className={styles.author}>
-            <Image
-              src={data.img}
-              alt=""
-              width={40}
-              height={40}
-              className={styles.avatar}
-            />
-            <span className={styles.username}>{data.username}</span>
-          </div>
-        </div>
-        <div className={styles.imageContainer}>
-          <Image
-            src={data.img}
-            alt=""
-            fill={true}
-            className={styles.image}
-          />
-        </div>
-      </div>
-      <div className={styles.content}>
-        <p className={styles.text}>
-         {data.content}
-        </p>
-      </div>
-    </div>
-  );
-};
-
-export default BlogPost;
+export default page
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,38 +1,9 @@
-import React from "react";
-import styles from "./page.module.css";
-import Link from "next/link";
-import Image from "next/image";
-import getData from "@/components/GetData/GetData";
+import React from 'react'
 
-export const metadata = { 
-  title: 'Blog',
-  description: 'Blog Page',
-} 
-
-const Blog = async () => {
-  const data = await getData("");
+const page = () => {
   return (
-    <div className={styles.mainContainer}>
-      {data.map((item) => (
-        <Link href={`/blog/${item._id}`} className={styles.container} key={item._id}>
-          <div className={styles.imageContainer}>
-            <Image
-              src={item.img}
-              alt=""
-              width={400}
-              height={250}
-              className={styles.image}
-            />
-          </div>
-          <div className={styles.content}>
-            <h1 className={styles.title}>{item.title}</h1>
-            <h2 className={styles.title}>{item.username}</h2>
-            <p className={styles.desc}>{item.desc}</p>
-          </div>
-        </Link>
-      ))}
-    </div>
-  );
-};
+    <div>page</div>
+  )
+}
 
-export default Blog;
+export default page
